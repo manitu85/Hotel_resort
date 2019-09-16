0bdcc43868e36a493a66aefd6b2ce52a9b6936e9
manitu85 <manitu.psy@gmail.com>
2019-09-16 18:12:58 +0200

    #

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Rooms from './pages/Rooms'
 import SingleRoom from './pages/SingleRoom'
 import Error from './pages/Error'
 
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './styles/app.scss'
+
 const App = () => {
   return (
     <div>
