8fcc6a26c33cadc0d0f5c4d34c79713593df1972
manitu85 <manitu.psy@gmail.com>
2019-09-18 00:16:53 +0200

    #

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Hero from '../components/Hero'
 
 const Home = () => {
   return (
     <div>
-      Hello frome home page
+     <Hero hero='defaultHero' />
     </div>
   )
 }
