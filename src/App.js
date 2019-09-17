b0ab76342143b3803d22ad4f7a9d93c89681cea2
manitu85 <manitu.psy@gmail.com>
2019-09-17 18:24:02 +0200

    #

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+import Navbar from './components/Navbar';
 import Home from './pages/Home'
 import Rooms from './pages/Rooms'
 import SingleRoom from './pages/SingleRoom'
@@ -8,15 +9,19 @@ import ErrorPage from './pages/Error'
 
 
 
+
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/rooms' component={Rooms} />
-        <Route exact path='/rooms/:roomId' component={SingleRoom} />
-        <Route path='*' component={ErrorPage} />
-      </Switch>
+      <>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/rooms' component={Rooms} />
+          <Route exact path='/rooms/:roomId' component={SingleRoom} />
+          <Route path='*' component={ErrorPage} />
+        </Switch>
+      </>
     </Router>
   );
 }
