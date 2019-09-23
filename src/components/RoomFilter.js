d33c3311e0d862e62d6f71577a3879e046755fcb
manitu85 <manitu.psy@gmail.com>
2019-09-23 18:00:57 +0200

    #

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -10,7 +10,7 @@ const getUniqueValue = (items, value) => {
 
 const RoomFilter = ({rooms}) => {
   const context = useContext(RoomContext);
-  const {
+  let {
     handleChange,
     type,
     capacity,
@@ -31,7 +31,11 @@ const RoomFilter = ({rooms}) => {
     return <option key={idx} value={item}>{item}</option>
   })
 
-
+  // get guests
+  let people = getUniqueValue(rooms, 'capacity')
+  people = people.map((item, idx) => {
+    return <option key={idx} value={item}>{item}</option>
+  })
 
 
   return (
@@ -52,9 +56,86 @@ const RoomFilter = ({rooms}) => {
           </select>
         </div>
         {/* end select type */}
+        {/* people */}
+        <div className='form-group'>
+          <label htmlFor='capacity'></label>
+          <select 
+            name='capacity' 
+            id='capacity' 
+            value={capacity} 
+            className='form-control'
+            onChange={handleChange}
+          >
+            { people }
+          </select>
+        </div>
+        {/* end people*/}
+        {/* room price */}
+        <div className='form-group'>
+          <label htmlFor='price'>room price &euro;{price}</label>
+          <input 
+            type='range'
+            name='price'
+            min={minPrice}
+            max={maxPrice}
+            id='price'
+            value={price}
+            onChange={handleChange}
+            className='form-control'
+          />
+        </div>
+        {/* end room price */}
+        {/* size */}
+        <div className="form-group">
+          <label htmlFor="size">room size</label>
+          <div className="size-inputs">
+            <input
+              type="number"
+              name="minSize"
+              id="size"
+              value={minSize}
+              onChange={handleChange}
+              className="size-input"
+            />
+            <input
+              type="number"
+              name="maxSize"
+              id="size"
+              value={maxSize}
+              onChange={handleChange}
+              className="size-input"
+            />
+          </div>
+        </div>
+        {/* end of size */}
+        {/* extras  */}
+        <div className="form-group">
+          <div className="single-extra">
+            <input
+              type="checkbox"
+              name="breakfast"
+              id="breakfast"
+              checked={breakfast}
+              onChange={handleChange}
+            />
+            <label htmlFor="breakfast">breakfast</label>
+          </div>
+          <div className="single-extra">
+            <input
+              type="checkbox"
+              name="pets"
+              id="pets"
+              checked={pets}
+              onChange={handleChange}
+            />
+            <label htmlFor="pets">pets</label>
+          </div>
+        </div>
+        {/* end of extras  */}
       </form>
     </section>
   );
 };
+
 
 export default RoomFilter;
--- src/context.js
+++ src/context.js
@@ -10,7 +10,7 @@ class RoomProvider extends Component {
     featuredRooms: [],
     loading: true,
     type: 'all',
-    capacity: 1,
+    capacity: 0,
     price: 0,
     minPrice: 0,
     maxPrice: 0,
@@ -19,6 +19,7 @@ class RoomProvider extends Component {
     breakfase: false,
     pets: false
   };
+
 
   componentDidMount() {
     let rooms = this.formatData(items)
@@ -62,25 +63,46 @@ class RoomProvider extends Component {
   handleChange = e => {
     const target = e.target;
     const value = target.type === "checkbox" ? target.checked : target.value;
-    const name = e.target.name;
+    const name = target.name;
 
     this.setState(
-      {
-        [name]: value
-      },
+      {[name]: value},
       this.filterRooms
     );
   };
 
   filterRooms = () => {
-    let { rooms, type, capacity, price, minSize, maxSize, breakfast, pets } = this.state;
+    let { rooms, type, capacity, price, minSize, maxSize, breakfast, pets } = this.state
     // all the rooms
-    let tempRooms = [...rooms];
+    let tempRooms = [...rooms]
+    // Transform the values
+    capacity = parseInt(capacity)
+    price = parseInt(price)
 
     // filter by type
-    if (type !== "all") {
+    if(type !== "all") {
       tempRooms = tempRooms.filter(room => room.type === type);
     }
+
+    // filter by capacity
+    if(capacity !==1) {
+      tempRooms = tempRooms.filter(room => room.capacity >= capacity)
+    }
+
+    // filter by price
+    tempRooms = tempRooms.filter(room => room.price <= price)
+
+    // filter by size
+    tempRooms = tempRooms.filter(room => room.size >= minSize && room.size <= maxSize  )
+
+     // filter by extras (breakfast and pets)
+     if(breakfast) {
+       tempRooms = tempRooms.filter(room => room.breakfast === true)
+     }
+
+     if(pets) {
+       tempRooms = tempRooms.filter(room => room.pets === true)
+     }
    
     // change state
     this.setState({
